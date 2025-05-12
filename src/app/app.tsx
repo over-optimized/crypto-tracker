fe48b7017dd5308c677b6e0cf2e45c80ff20aa82
Lee Spence <leespencedev@gmail.com>
2025-05-11 20:44:51 -0700

    extract transaction api hook

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,39 +1,11 @@
 // Uncomment this line to use CSS modules
 // import styles from './app.module.css';
-import { useEffect, useState } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
-import { transactionApi, TransactionResponse } from 'src/apis/transactionApi';
 import { TransactionTable } from 'src/components/TransactionTable/TransactionTable';
-import { tryCatch } from 'src/utils/tryCatch';
+import { useTransactionApi } from 'src/hooks/useTransactionApi';
 
 export function App() {
-  const [state, setState] = useState({
-    transactions: [] as TransactionResponse,
-  });
-  useEffect(() => {
-    (async () => {
-      const { error, data } = await tryCatch(
-        transactionApi({
-          fileName: 'BTC-account-statement_2025-04-01_2025-05-01.csv',
-        })
-      );
-
-      if (error) {
-        console.error('Error fetching transactions:', error);
-        return;
-      }
-
-      if (data) {
-        console.log('Fetched transactions:', data);
-        setState((prevState) => ({
-          ...prevState,
-          transactions: data,
-        }));
-      }
-    })();
-  }, []);
-
-  console.log('State:', state);
+  const { state } = useTransactionApi();
 
   return (
     <div>
---
+++ src/hooks/useTransactionApi.ts
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import { transactionApi, TransactionResponse } from 'src/apis/transactionApi';
+import { tryCatch } from 'src/utils/tryCatch';
+
+export function useTransactionApi() {
+  const [state, setState] = useState<{
+    transactions: TransactionResponse;
+    error: string | null;
+  }>({
+    transactions: [],
+    error: null,
+  });
+
+  (async (fileName: string) => {
+    try {
+      const { data, error } = await tryCatch(transactionApi({ fileName }));
+      if (error) {
+        throw error;
+      }
+      setState((prevState) => ({
+        ...prevState,
+        transactions: data,
+      }));
+    } catch (error) {
+      setState((prevState) => ({
+        ...prevState,
+        error: error as string,
+      }));
+    }
+  })('BTC-account-statement_2025-04-01_2025-05-01.csv');
+
+  return { state };
+}
