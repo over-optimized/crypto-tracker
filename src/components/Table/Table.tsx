f35864a21c78bebc131b2074c9b3b43fb11b424c
Lee Spence <leespencedev@gmail.com>
2025-05-14 21:41:12 -0700

    add transaction controls to table component

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { TransactionResponse } from 'src/apis/transactionApi';
 import styles from './Table.module.css';
 import { createHeaderData } from './createHeaderData';
@@ -10,7 +10,14 @@ type TableProps = {
 };
 
 export function Table({ data }: TableProps) {
-  console.log('Table data:', data);
+  const [state] = useState({
+    controls: {
+      currency: '',
+      transactionType: '',
+      status: '',
+    },
+  });
+  // console.log('Table data:', data);
   const headers = createHeaderData(data[0]);
   const rows = useMemo(
     () =>
@@ -25,6 +32,49 @@ export function Table({ data }: TableProps) {
   }
   return (
     <div>
+      <p># Transactions: {data.length}</p>
+      <div className={styles.tableControls}>
+        <div className="ml-4">
+          <label>
+            <span>Currency </span>
+            <select
+              name="currency-dropdown"
+              id="currency-dropdown"
+              value={state.controls.currency}
+            >
+              <option value=""></option>
+              <option value="btc">BTC</option>
+            </select>
+          </label>
+        </div>
+        <div className="ml-4">
+          <label>
+            <span>Transaction type </span>
+            <select
+              name="transaction-type-dropdown"
+              id="transaction-type-dropdown"
+              value={state.controls.transactionType}
+            >
+              <option value=""></option>
+              <option value="exchange">Exchange</option>
+              <option value="on-chain">On-Chain</option>
+            </select>
+          </label>
+        </div>
+        <div className="ml-4">
+          <label>
+            <span>Status </span>
+            <select
+              name="transaction-status-dropdown"
+              id="transaction-status-dropdown"
+              value={state.controls.status}
+            >
+              <option value=""></option>
+              <option value="completed">Completed</option>
+            </select>
+          </label>
+        </div>
+      </div>
       <table className={styles.table}>
         <thead>
           <tr>{headers}</tr>
